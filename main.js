170120fa8e239fa2b81c880e63bd76820b0438f1
ivantarabanovych <vanjokcitet334@gmail.com>
2024-10-03 00:43:07 +0300

    q4

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-function delay(seconds){
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, seconds * 1000);
-    });
-}
-
-async function delayAction() {
-    console.log('Очікування...');
-    await delay(3);
-    console.log('3 секунди минули!');
-}
-
-delayAction();
+// Коли використовувати async / await: Якщо код містить послідовні асинхронні операції (наприклад, кілька послідовних запитів до API), використання async / await робить код простішим для читання і розуміння. Це дозволяє уникати вкладеності обіцянок і робить асинхронний код схожим на синхронний.

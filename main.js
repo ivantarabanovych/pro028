8e99f66ce31e504f251c2374cba6604e8ba8fed1
ivantarabanovych <vanjokcitet334@gmail.com>
2024-10-03 01:02:41 +0300

    q5

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// Коли використовувати async / await: Якщо код містить послідовні асинхронні операції (наприклад, кілька послідовних запитів до API), використання async / await робить код простішим для читання і розуміння. Це дозволяє уникати вкладеності обіцянок і робить асинхронний код схожим на синхронний.
+async function loadImage() {
+    try{
+        const responce = await fetch('https://images.unsplash.com/photo-1461988320302-91bde64fc8e4?')
+        if (!responce.ok) {
+            throw new Error('Помилка сервера')
+        }
+        const img = document.createElement('img');
+        img.src = responce.url;
+        img.alt = 'Some image'
+        img.width = 600;
+
+        const image = document.getElementById('image');
+        image.innerHTML = '';
+        image.appendChild(img);
+    } catch (error){
+        console.error('Error loading image', error);
+    }
+}
+
+document.getElementById('load-image').addEventListener('click', loadImage);

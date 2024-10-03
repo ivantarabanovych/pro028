6ddf6025f06ab00093b37769db48925a89d52c09
ivantarabanovych <vanjokcitet334@gmail.com>
2024-10-04 00:09:04 +0300

    q8

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-async function getResultFromPromise() {
+async function requestData() {
     try{
-        const result = await promiseFunction();
-        console.log(result);
+        const responce1 = await fetch('https://api.example.com/data1');
+        const data1 = await responce1.json();
+        console.log('Дані з першого запиту', data1);
+        
+        const responce2 = await fetch('https://api.example.com/data2/${data1.id}');
+        const data2 = await responce2.json();
+        console.log('Дані з другого запиту', data2);
     } catch (error){
-        console.error('Error', error);
+        console.error('Помилка під час виконання запитів', error);
     }
 }
+
+requestData();
